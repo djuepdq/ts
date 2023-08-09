7f13df4ed0e7b0331c3bd51860d7d30f8d6f3f9f
Nikita Voloboev <nikita.voloboev@gmail.com>
2023-08-09 03:19:10 +0200

    .

--- lib/safari.ts
+++ lib/safari.ts
@@ -92,3 +92,15 @@ if (fs.existsSync(filePath)) {
 // const file = Bun.file(file_path)
 // const linksParsed = JSON.parse(await file.text())
 // console.log(linksParsed)
+
+// TODO: attempt to get safari url
+// TODO: write using JXA approach above and delete below code
+// import { execa } from "execa"
+// TODO: need https://github.com/dsherret/dax but for bun..
+// https://github.com/google/zx can't import
+// https://github.com/sindresorhus/execa not nice DX but can't do below string with "" to work using it
+// Return URL of current tab in Safari
+// export async function getSafariUrl() {
+//   // const { stdout } = await execa("osascript", ['-e'], '')
+//   // return await $` -e 'tell application "Safari" to return URL of front document'`
+// }
