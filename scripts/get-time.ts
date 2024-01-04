73b456c184f5176860cbebace692f751bf5bf58e
Nikita Voloboev <nikita.voloboev@gmail.com>
2024-01-04 12:22:57 +0300

    .

---
+++ scripts/get-time.ts
@@ -0,0 +1,19 @@
+// @ts-ignore
+const args = Bun.argv
+const timezone = args[2]
+
+if (timezone) {
+  console.log(getCurrentTime(timezone))
+}
+
+function getCurrentTime(timezone: "sf") {
+  const date = new Date()
+  const options: Intl.DateTimeFormatOptions = {
+    timeZone: "America/Los_Angeles",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false,
+  }
+  const formatter = new Intl.DateTimeFormat("en-US", options)
+  return `Time in SF: ${formatter.format(date)}`
+}
