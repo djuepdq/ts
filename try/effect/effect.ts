9630c6dfe5a6fcdf8a77ecb077a4368db112171f
Nikita Voloboev <nikita.voloboev@gmail.com>
2024-02-16 13:58:03 +0300

    .

---
+++ try/effect/effect.ts
@@ -0,0 +1,9 @@
+import { Effect } from "effect"
+
+const divide = (a: number, b: number): Effect.Effect<number, Error> =>
+  b === 0
+    ? Effect.fail(new Error("Cannot divide by zero"))
+    : Effect.succeed(a / b)
+
+const res = divide(10, 2)
+console.log(res)
